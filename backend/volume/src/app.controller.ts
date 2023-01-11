7786b0afafb9af858e715740786c9661ea204fde
oswin bult <oswin.bult@live.nl>
2023-01-11 16:17:19 +0100

    add the backend folder init

---
+++ backend/volume/src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+
+  @Get('cat')
+  getCat(): string {
+  	return 'cat';
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
