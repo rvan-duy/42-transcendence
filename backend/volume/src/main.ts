e6b84c923078f6c395c5aaed9eb51ab8147219ed
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-01-30 21:27:10 +0100

    allow more CORS functionality
    As per the previous commit, the host machine's IP address will be added to allowed origins for CORS by having the docker-compose file pass it as an environment variable. Every codam dns (e.g. f0r1s1.codam.nl) will also be allowed on the CORS list (this is currently hardcoded with a triple for-loop). Accessing environment variables requires installing @types/node in the back-end (otherwise it will complain about process.env in main.ts). This still needs to be set up in the dockerfile.

--- backend/volume/src/main.ts
+++ backend/volume/src/main.ts
@@ -8,12 +8,41 @@ import { PrismaService } from './prisma/prisma.service';
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const prismaService = app.get(PrismaService)
-  await prismaService.enableShutdownHooks(app)
-  app.enableCors(    { 
-    origin: ['http://localhost:8000'],
-  });
-  await app.listen(3000)
+	const app = await NestFactory.create(AppModule);
+	const prismaService = app.get(PrismaService)
+	await prismaService.enableShutdownHooks(app)
+
+	// If IP variable is set in ENV, this will add it as host.
+	const server_host = "http://" + process.env["SERVER_HOST"] + ":8000" ;
+	console.log("Server host: ", server_host);
+	
+	// Index all floor/row/seat addresses for Codam imac DNSs.
+	let codam_dns: string[] = ['http://127.0.0.1:8000'];
+	for (var f=0; f<2; f++)
+	{
+		for(var r=1; r<7; r++)
+		{
+			for(var s=1; s<30; s++)
+			{
+				const dns_for_imac = "http://f"+f+"r"+r+"s"+s+".codam.nl:8000";
+				codam_dns.push(dns_for_imac);
+			}
+		}
+	}
+	
+	// Allow CORS for all the above, and localhost/127.0.0.1
+	let origin_array = ['http://localhost:8000', 'http://127.0.0.1:8000', server_host];
+	origin_array = origin_array.concat(codam_dns);
+	// console.log(origin_array);
+	
+	app.enableCors({
+	origin: origin_array,
+	//Default vals if not specified:
+	"methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+	"preflightContinue": false,
+	"optionsSuccessStatus": 204
+	}
+	);
+	await app.listen(3000)
 }
 bootstrap()
