e3b8c3e31f031267aebaa72076cd8f40ea397dc0
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-01-31 16:09:49 +0100

    testttdd

--- frontend/rubenpong/.eslintrc.cjs
+++ frontend/rubenpong/.eslintrc.cjs
@@ -4,7 +4,12 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    'plugin:vue/vue3-recommended',
+    "plugin:vue/vue3-essential",
+    // 'plugin:vue/vue3-recommended',
+
+    // "eslint:recommended",
+    // "@vue/eslint-config-typescript",
+    // "@vue/eslint-config-prettier",
   ],
   // rules: {
   //   indent: ["error", 2], // 2 spaces
