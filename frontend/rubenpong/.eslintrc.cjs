177943425d338d206451c4d8f9a31295541e296d
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-01-31 16:19:10 +0100

    update linter file

--- frontend/rubenpong/.eslintrc.cjs
+++ frontend/rubenpong/.eslintrc.cjs
@@ -4,17 +4,10 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    // 'plugin:vue/vue3-recommended',
-
-    // "eslint:recommended",
-    // "@vue/eslint-config-typescript",
-    // "@vue/eslint-config-prettier",
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/eslint-config-typescript',
   ],
-  // rules: {
-  //   indent: ["error", 2], // 2 spaces
-  //   "no-unused-vars": "error",
-  // },
   parserOptions: {
     ecmaVersion: "latest",
   },
