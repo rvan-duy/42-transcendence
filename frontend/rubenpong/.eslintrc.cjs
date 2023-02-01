5d1758bf709e464843550f5747b00cb3de48ae98
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-02-01 12:01:50 +0100

    changed some config options for the linter

--- frontend/rubenpong/.eslintrc.cjs
+++ frontend/rubenpong/.eslintrc.cjs
@@ -3,13 +3,12 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
+  extends: [
     'eslint:recommended',
+    'plugin:vue/vue3-essential',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
+    ecmaVersion: "latest",
+  },
 }
