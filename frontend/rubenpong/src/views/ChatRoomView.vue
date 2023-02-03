33bcb5479b8947924850ccb5c3ef0038ecdf698f
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-03 20:09:42 +0100

    create a separate page for actual chat rooms
    This html has been copied from an online tutorial as a starting point to follow along with. Also included a css file to tyle both the chat room and the chat landing page from this tutorial.

---
+++ frontend/rubenpong/src/views/ChatRoomView.vue
@@ -0,0 +1,69 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="chat">
+    <!-- <h1 id="chat_text">This is the chat page (for now)</h1> -->
+	<body>
+		<div class="chat-container">
+			<header class="chat-header">
+			<h1><i class="fas fa-smile"></i> ChatCord</h1>
+			<a href="index.html" class="btn">Leave Room</a>
+			</header>
+			<main class="chat-main">
+			<div class="chat-sidebar">
+				<h3><i class="fas fa-comments"></i> Room Name:</h3>
+				<h2 id="room-name">Room 1</h2>
+				<h3><i class="fas fa-users"></i> Users</h3>
+				<ul id="users">
+				<li>Brad</li>
+				<li>John</li>
+				<li>Mary</li>
+				<li>Paul</li>
+				<li>Mike</li>
+				</ul>
+			</div>
+			<div class="chat-messages">
+							<div class="message">
+								<p class="meta">Ruben 1<span>9:12pm</span></p>
+								<p class="text">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+									repudiandae.
+								</p>
+							</div>
+							<div class="message">
+								<p class="meta">Ruben 2<span>9:15pm</span></p>
+								<p class="text">
+									Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+									repudiandae.
+								</p>
+							</div>
+			</div>
+			</main>
+			<div class="chat-form-container">
+			<form id="chat-form">
+				<input
+				id="msg"
+				type="text"
+				placeholder="Enter Message"
+				required
+				autocomplete="off"
+				/>
+				<button class="btn"><i class="fas fa-paper-plane"></i> Send</button>
+			</form>
+			</div>
+		</div>
+	</body>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .chat {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
