ad16254a347c59c9f397effd92fde13c3ab95a90
Oswin Bult <obult@student.codam.nl>
2023-02-03 12:23:24 +0100

    add new services to interact with the current prisma schema

--- backend/volume/src/prisma/prisma.module.ts
+++ backend/volume/src/prisma/prisma.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { PostService } from './post.service';
 import { UserService } from './user.service';
 import { PrismaService } from './prisma.service'
 
 @Module({
   imports: [PrismaClient],
   controllers: [],
-  providers: [UserService, PostService, PrismaService],
-  exports: [UserService, PostService, PrismaService],
+  providers: [UserService, PrismaService],
+  exports: [UserService, PrismaService],
 })
 export class PrismaModule {}
