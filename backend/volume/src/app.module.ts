a19ba482f13f094a51b6fb372c75177fa8173bf9
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-06 14:24:16 +0100

    add gateways to backend
    Nestjs handles WebSockets through a class called a 'Gateway' (whereas most tutorials/frameworks seem to just use socket.io, Nestjs needs a Gateway to wrap around socket.io or any other websocket platform). This commit installs the required gateway package (updated package.json) and sets up a few typescript files where Gateways can be expanded to a useable class and module for the rest of the project.

--- backend/volume/src/app.module.ts
+++ backend/volume/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './controllers/user/userController.service';
+import { GatewayModule } from './gateway/gateway.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, GatewayModule],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
