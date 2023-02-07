e37b416f01999eac32ab353323f13358c238fea7
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-07 13:34:32 +0100

    delete useless junk from main.ts in backend
    we have no need for random codam imacs to be individually listed in the cors whitelist.

--- backend/volume/src/main.ts
+++ backend/volume/src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { PrismaService } from './services/prisma/prisma.service';
 import { PrismaService } from './prisma/prisma.service';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config();
