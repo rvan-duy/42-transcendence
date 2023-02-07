2a1604d55944a9e4a6d3c3e321dd99ed6c928d19
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-07 13:34:32 +0100

    delete useless junk from main.ts in backend
    we have no need for random codam imacs to be individually listed in the cors whitelist.

--- backend/volume/src/main.ts
+++ backend/volume/src/main.ts
@@ -2,14 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 // import { PrismaService } from './services/prisma/prisma.service';
 import { PrismaService } from './prisma/prisma.service';
-// var cors = require('cors')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const prismaService = app.get(PrismaService);
-  await prismaService.enableShutdownHooks(app);
-  app.enableCors( {
-    origin: ['http://localhost:8000', 'http://codam.nl:8000', 'http://f0r6s9.codam.nl:8000', 'http://f0r5s9.codam.nl:8000'],
+  const prismaService = app.get(PrismaService)
+  await prismaService.enableShutdownHooks(app)
+  app.enableCors(    { 
+    origin: ['http://localhost:8000', 'http://localhost:3000'],
   });
   await app.listen(3000);
 }
