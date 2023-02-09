3b2567f4365bf7f732d89402e6b9b37ebeb3336b
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-09 14:25:45 +0100

    allow socket connections between frontend and backend
    We now have some minimal kind of connection between frontend and backend (we can register when a client connects, and log it in our backend). The way we do this is still kind of scuffed - the socketio service in the frontend doesn't use types despite being a typescript file, the frontend starts polling for connections before the backend even starts up so generates 3 or 4 errors before succeeding, etc. Also we completely disabled CORS lmao (which we may want to re-enable at some point before 'production' - aka when we hand it in).

---
+++ backend/volume/src/chat/chat.gateway.ts
@@ -0,0 +1,66 @@
+import {
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	OnGatewayInit,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
+	WsResponse } from '@nestjs/websockets';
+  import { Server, Socket } from 'socket.io';
+  import * as moment from 'moment';
+
+@WebSocketGateway({
+	cors: {
+		origin: '*',
+	}
+	namespace: '/chat'
+})
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+	@WebSocketServer() all_clients: Server; //all clients
+	
+	@SubscribeMessage('msgToServer')
+	handleMessage(client: Socket, text: string) {
+	  console.log(`Server received msg: "${text}" from client: ${client.id}`);
+	  this.all_clients.emit('msgToClient', this.formatMessage('USER', text));
+	}
+  
+	afterInit(server: Server) {
+	  console.warn(server + ' is unused');
+	  console.log('Gateway initialised.');
+	}
+  
+	handleConnection(client: Socket, ...args: any[]) {
+	  client.broadcast.emit('msgToClient', this.formatMessage('Rubot','A new user joined the chat.')); //all OTHER clients
+	  client.emit('msgToClient', this.formatMessage('Rubot','Welcome to the chat!')); //this client
+	  // console.warn(args + ' is unused');
+	  console.log(`Client ${client.id} connected`);
+  
+	  args.forEach(element => {
+		  console.log('Arg passed to connection: ');
+		  console.log(element)
+	  });
+  
+	  // const qs_import = 'query-string';
+	  // import (qs_import).then( (queryString) => {
+	  // 	const { user_name, room_name } = queryString.parse(location.search, {
+	  // 	// ignoreQueryPrefix: true
+	  // 	});
+	  // 	console.log(user_name, room_name);
+	  // });
+	}
+  
+	handleDisconnect(client: Socket) {
+	  this.all_clients.emit('msgToClient', this.formatMessage('Rubot','A user left the chat.')); //all clients
+	  console.log(`Client ${client.id} disconnected`);
+	}
+  
+	formatMessage(username, message_body)
+	{
+	  return {
+		  username: username,
+		  body: message_body,
+		  time: moment().format('HH:mm ZZ') //TODO: Make not GMT maybe?
+	  }
+	}
+  
+  }
