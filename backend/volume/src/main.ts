3cd48ea9a9f9d4172f6e342218c10977ccd9a998
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-09 15:40:27 +0100

    run the linter on the socket code
    A bunch of unused variable warnings were silenced with console.warn('insert unused variable here' + ' is unused') lmao.

--- backend/volume/src/main.ts
+++ backend/volume/src/main.ts
@@ -5,9 +5,9 @@ import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const prismaService = app.get(PrismaService)
-  await prismaService.enableShutdownHooks(app)
-  app.enableCors(    { 
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+  app.enableCors( {
     origin: ['*'],
   });
   await app.listen(3000);
--- frontend/rubenpong/src/services/socketio.service.ts
+++ frontend/rubenpong/src/services/socketio.service.ts
@@ -1,17 +1,18 @@
-import { io, Socket } from 'socket.io-client';
+// import { Socket } from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 class SocketioService {
-	// socket: Socket;
-	socket: any;
+  // socket: Socket;
+  socket: any;
 
-	constructor() {
-		// this.socket = io('http://localhost:3000'); //TODO: Replace with .env variable like 'process.env.VUE_APP_SOCKET_ENDPOINT'
-	}
+  constructor() {
+    // this.socket = io('http://localhost:3000'); //TODO: Replace with .env variable like 'process.env.VUE_APP_SOCKET_ENDPOINT'
+  }
 
-	setupSocketConnection() {
-		this.socket = io('http://localhost:3000'); //TODO: Replace with .env variable like 'process.env.VUE_APP_SOCKET_ENDPOINT'
-		console.log("Set up socket between localhost:3000 and frontend");
-	}
+  setupSocketConnection() {
+    this.socket = io('http://localhost:3000'); //TODO: Replace with .env variable like 'process.env.VUE_APP_SOCKET_ENDPOINT'
+    console.log('Set up socket between localhost:3000 and frontend');
+  }
 
 }
 
