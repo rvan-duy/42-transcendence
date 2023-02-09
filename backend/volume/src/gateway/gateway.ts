8f7a0de69f502958bb5c3f3782af1729f4c2629f
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-09 15:40:27 +0100

    run the linter on the socket code
    A bunch of unused variable warnings were silenced with console.warn('insert unused variable here' + ' is unused') lmao.

--- backend/volume/src/gateway/gateway.ts
+++ backend/volume/src/gateway/gateway.ts
@@ -64,37 +64,41 @@ export class MyGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayD
 
 }
 // import { OnModuleInit } from "@nestjs/common";
-import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
+// import { MessageBody } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer, WsResponse } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway({
-	cors: {
-		origin: '*',
-	}
+  cors: {
+    origin: '*',
+  }
 })
 export class MyGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-	@WebSocketServer() broadcast: Server;
-	
-	@SubscribeMessage('msgToServer')
-	handleMessage(client: Socket, text: string): WsResponse<string> {
-		// client.emit('msgToClient', text);
-		// this.broadcast.emit('msgToClient', text);
-		return { event: 'msgToClient', data: text};
-	}
-	
-	// Implemented method overrides //
-	afterInit(server: Server) {
-		console.log("Gateway initialised.")
-	}
+  @WebSocketServer() broadcast: Server;
 
-	handleConnection(client: Socket, ...args: any[]) {
-		console.log(`Client ${client.id} connected`);
-	}
-	
-	handleDisconnect(client: Socket) {
-		console.log(`Client ${client.id} disconnected`);
-	}
-	// //////////////////////////// //
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): WsResponse<string> {
+    console.warn(client + ' is unused');
+    // client.emit('msgToClient', text);
+    // this.broadcast.emit('msgToClient', text);
+    return { event: 'msgToClient', data: text};
+  }
+
+  // Implemented method overrides //
+  afterInit(server: Server) {
+    console.warn(server + ' is unused');
+    console.log('Gateway initialised.');
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    console.warn(args + ' is unused');
+    console.log(`Client ${client.id} connected`);
+  }
+
+  handleDisconnect(client: Socket) {
+    console.log(`Client ${client.id} disconnected`);
+  }
+// //////////////////////////// //
 }
 
 // @WebSocketGateway(/*{  cors: true }*/)
