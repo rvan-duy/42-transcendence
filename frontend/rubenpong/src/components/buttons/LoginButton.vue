7f7af0d1755ee1d72e4beb839219c4d10c323fe6
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-02-09 14:45:05 +0100

    init login button redirecting to 42 api

---
+++ frontend/rubenpong/src/components/buttons/LoginButton.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <button>
+    <a href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-77defdf5da75714147831f8ce2531c3aa1554a9e79226dc62041558ded39460b&redirect_uri=http%3A%2F%2Flocalhost%3A8000&response_type=code">
+      Login
+    </a>
+  </button>
+</template>
