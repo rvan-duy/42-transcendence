ab16404ef9780060a88cf83c5379de9866713ac1
Oswin Bult <obult@student.codam.nl>
2023-02-16 14:35:10 +0100

    add what ruben wants to have

---
+++ backend/volume/src/auth/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaUserService } from '../user/prisma/prismaUser.service'
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: PrismaUserService) {}
+
+  get_or_create(intraId: number, name: string) {
+    let currentUser = this.userService.user({intraId: intraId});
+    if (currentUser)
+      return currentUser;
+    return this.userService.createUser({
+      intraId: intraId,
+      name: name,
+    })
+  }
+}
