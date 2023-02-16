8afdf6dbe77cddfd2dd46f49b6632f85639089eb
Oswin Bult <obult@student.codam.nl>
2023-02-16 14:36:42 +0100

    lint

--- backend/volume/src/auth/auth.service.ts
+++ backend/volume/src/auth/auth.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaUserService } from '../user/prisma/prismaUser.service'
+import { PrismaUserService } from '../user/prisma/prismaUser.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: PrismaUserService) {}
 
   get_or_create(intraId: number, name: string) {
-    let currentUser = this.userService.user({intraId: intraId});
+    const currentUser = this.userService.user({intraId: intraId});
     if (currentUser)
       return currentUser;
     return this.userService.createUser({
       intraId: intraId,
       name: name,
-    })
+    });
   }
 }
