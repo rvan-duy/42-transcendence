cf603a21db3919b2a5fc0c96c32fb7f67f4ad169
Oswin Bult <obult@student.codam.nl>
2023-02-20 14:36:40 +0100

    game gateway init

--- backend/volume/src/game/game.gateway.ts
+++ backend/volume/src/game/game.gateway.ts
@@ -1,5 +1,29 @@
-import { WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { SubscribeMessage, WebSocketGateway, OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect } from '@nestjs/websockets';
+import { Game } from '@prisma/client';
 import { Socket, Server } from 'socket.io';
+
+enum GameMode {
+  SURVIVAL = "Survival",
+  CREATIVE = "Creative",
+  ADVENTURE = "Adventure",
+  SPECTATOR = "Spectator",
+}
+
+
+class Player {
+  userId: number;
+  batXY: number[];
+  score: number;
+  acceleration: number;
+}
+
+class Games {
+  player: Player[];
+  watchers: number[];
+  mapsize: number[];
+  mode: GameMode;
+  texturePath: string;
+}
 
 @WebSocketGateway({
   cors: {
@@ -9,6 +33,7 @@ import { Socket, Server } from 'socket.io';
 })
 export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private server: Server;
+  private games: Games[];
 
   afterInit(server: Server) {
     this.server = server;
@@ -22,4 +47,17 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   handleDisconnect(client: Socket) {
     console.log(`Client disconnected: ${client.id}`);
   }
+
+  @SubscribeMessage('up')
+  handleUp(client: any, payload: any): string {
+    console.log('Received payload:', payload);
+    return 'Server says hello!';
+  }
+
+  @SubscribeMessage('down')
+  handleDown(client: any, payload: any): string {
+    console.log('Received payload:', payload);
+    return 'Server says hello!';
+  }
+
 }
