0947c0b4bf7d9bd12c640118b17873b49a7ccd77
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-02-21 10:56:02 +0100

    remove @Obult 's meuk

--- frontend/rubenpong/src/router/index.ts
+++ frontend/rubenpong/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,22 +15,27 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: () => import('@/views/AboutView.vue'),
     },
     {
       path: '/chat',
       name: 'chat',
-      component: () => import('../views/ChatView.vue'),
+      component: () => import('@/views/ChatView.vue'),
     },
     {
       path: '/chatroom',
       name: 'chatroom',
-      component: () => import('../views/ChatRoomView.vue'),
+      component: () => import('@/views/ChatRoomView.vue'),
     },
     {
-      path: '/canvas', //waarom staat deze meuk nou weer hier he Oswin >:(
-      name: 'canvas',
-      component: () => import('../views/CanvasView.vue'),
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LogInView.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'not-found',
+      component: () => import('@/views/NotFoundView.vue'),
     },
   ],
 });
