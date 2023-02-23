545e03f86295e32822353a4f074e897e71ceb989
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-02-23 16:01:32 +0100

    Restore functionality to chat page
    The chat stopped working after concluding the merge, this was mainly due to a module not being exported and imported properly.

--- frontend/rubenpong/src/services/socketio.service.ts
+++ frontend/rubenpong/src/services/socketio.service.ts
@@ -11,8 +11,13 @@ class SocketioService {
   }
 
   setupSocketConnection() {
-    this.socket = io('http://localhost:3000'); //TODO: Replace with .env variable like 'process.env.VUE_APP_SOCKET_ENDPOINT'
-    console.log('Set up socket between localhost:3000 and frontend');
+    this.socket = io('http://localhost:3000/chat'); //TODO: Replace with .env variable like 'process.env.VUE_APP_SOCKET_ENDPOINT'
+	this.socket.on("connect_error", (err: any) => {
+		console.log(`connect_error due to ${err.message}`);
+    });
+	this.socket.on('connect', () => {
+		console.log('Set up socket between localhost:3000/chat and frontend');
+    });
   }
 
 }
