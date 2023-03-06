2c8b122974f5bf1d8d913f19fc9475c85bbaccda
Dagmar Kramer <dkramer@f0r6s6.codam.nl>
2023-03-06 10:59:37 +0100

    new gameview van ruben

--- frontend/rubenpong/src/views/GameView.vue
+++ frontend/rubenpong/src/views/GameView.vue
@@ -1,6 +1,4 @@
-
 <script setup lang="ts">
-
 </script>
 
 <template>
@@ -19,13 +17,6 @@
 <script lang="ts">
 import io from 'socket.io-client';
 // import CurrentGameState from 'game.service'
-function increaseArraySize(inputArray: Uint8ClampedArray): Uint8ClampedArray {
-  const outputArray = new Uint8ClampedArray(64);
-  for (let i = 0; i < 64; i++) {
-    outputArray[i] = inputArray[i % 4];
-  }
-  return outputArray;
-}
 export default {
   mounted() {
     var init: boolean = false;
@@ -33,28 +24,18 @@ export default {
     var ctx: CanvasRenderingContext2D = canvas.getContext('2d') as CanvasRenderingContext2D;
     var queue: ImageData[];
     const socket: any = io('http://localhost:3000/game');
-    socket.on("connect_error", (err) => {
+    socket.on('connect_error', (err) => {
       console.log(`connect_error due to ${err.message}`);
     });
- 
-		// class GameData {
-    //   ball: number[] = [500, 300];
-    //   paddle1:  number[] = [0, 275];
-    //   paddle2:  number[] = [980, 275];
-    //   score:    number[] = [0, 0];
-    // }
-
 enum MapSize {
   WIDTH = 1000,
   HEIGHT = 600,
 }
-
 enum DefaultElementSize {
   PADDLEWIDTH = 20,
   PADDLEHEIGHT = 100,
   BALLRADIUS = 20,
 }
-
 class CurrentGameState {
   score:number[] = [0, 0];
   leftPaddleCoords: number[] = [0, MapSize.HEIGHT / 2];
@@ -66,178 +47,126 @@ class CurrentGameState {
   ballCoords: number[] = [MapSize.WIDTH / 2, MapSize.HEIGHT / 2];
   ballRadius: number = DefaultElementSize.BALLRADIUS;
 }
-
 socket.on('pos', (data: any) => {
-        const datas: CurrentGameState = data;
-        //draw background
-        ctx.fillStyle = 'black';
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.fillStyle = 'white';
-        if (datas.score[1] >= 10 || datas.score[0] >= 10 )
-        {
-          ctx.fillStyle = 'black';
-          ctx.fillRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle = 'white';
-          if (datas.score[1] >= 10 )
-            ctx.fillText('You won!', canvas.width / 2 - 100, canvas.height / 2);
-          if (datas.score[0] >= 10 )
-            ctx.fillText('You lost!', canvas.width /  2 - 100, canvas.height / 2);
-        }
-        //draw plateau other player
-        else if (datas.score[1] < 10 || datas.score[0] < 10) {
-          ctx.fillRect(datas.leftPaddleCoords[0], datas.leftPaddleCoords[1], datas.leftPaddleWidth, datas.leftPaddleHeight);
-          //draw plateau you
-          ctx.fillStyle = 'white';
-          ctx.fillRect(datas.rightPaddleCoords[0], datas.rightPaddleCoords[1], datas.rightPaddleWidth, datas.rightPaddleHeight);
-          //draw ball
-          ctx.fillStyle = 'red';
-          ctx.beginPath();
-          ctx.arc(datas.ballCoords[0], datas.ballCoords[1], datas.ballRadius, 0, Math.PI * 2, false);
-          ctx.closePath();
-          ctx.fill();
-          //draw text
-          ctx.fillStyle = 'white';
-          ctx.font = '50px arial';
-          ctx.fillText('other', canvas.width / 4, canvas.height / 8);
-          //draw text
-          ctx.fillStyle = 'white';
-          ctx.font = '50px arial';
-          ctx.fillText(datas.score[0].toString(), canvas.width / 4 + 40, canvas.height / 4);
-          //draw text
-          ctx.fillStyle = 'white';
-          ctx.font = '50px arial';
-          ctx.fillText('you', canvas.width / 4 * 3 - 100, canvas.height / 8);
-          //draw text
-          ctx.fillStyle = 'white';
-          ctx.font = '50px arial';
-          ctx.fillText(datas.score[1].toString(), canvas.width / 4 * 3 - 70, canvas.height / 4);
-          //draw net
-          for(let i = 0; i <= canvas.height; i+=15){
-            ctx.fillStyle = 'white';
-            ctx.fillRect(canvas.width / 2 - 1.5 , i, 3, 10);
-          }
-        }
-      });
-
-    // function render()
-    // {
-    //   socket.emit('pos');
-    // }
-
-    const ball: {x: number, y: number, speed: number, Xvelocity: number, Yvelocity: number, rad: number} = {
-      x: canvas.width/2,
-      y: canvas.height/2,
-      speed: 5,
-      Xvelocity: 5,
-      Yvelocity: 5,
-      rad: 20,
-    };
-
-    const plat: {x: number, y: number, height: number, width: number, score:number} = {
-      x: canvas.width - DefaultElementSize.PADDLEWIDTH,
-      y: canvas.height / 2 - DefaultElementSize.PADDLEHEIGHT / 2,
-      height: 100,
-      width: 20,
-      score: 0
-    };
-
-    const other: {x: number, y: number, height: number, width: number, score:number} = {
-		x: 0,
-		y : canvas.height / 2 - DefaultElementSize.PADDLEHEIGHT / 2,
-		height: 100,
-		width: 20,
-      score: 0
-    };
-
-    var arrowUp: Boolean = false;
-    var arrowDown: Boolean = false;
-    var arrowLeft: Boolean = false;
-    var arrowRight: Boolean = false;
-
-
-    function movePlat(e: KeyboardEvent)
+  const datas: CurrentGameState = data;
+  //draw background
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  //draw plateau player 1
+  ctx.fillStyle = 'white';
+  ctx.fillRect(datas.leftPaddleCoords[0], datas.leftPaddleCoords[1], datas.leftPaddleWidth, datas.leftPaddleHeight);
+  
+  //draw plateau player 2
+  ctx.fillStyle = 'white';
+  ctx.fillRect(datas.rightPaddleCoords[0], datas.rightPaddleCoords[1], datas.rightPaddleWidth, datas.rightPaddleHeight);
+  
+  //draw ball
+  ctx.fillStyle = 'red';
+  ctx.beginPath();
+  ctx.arc(datas.ballCoords[0], datas.ballCoords[1], datas.ballRadius, 0, Math.PI * 2, false);
+  ctx.closePath();
+  ctx.fill();
+  
+  //draw text player 1
+  ctx.fillStyle = 'white';
+  ctx.font = '50px arial';
+  ctx.fillText('Player 1', canvas.width / 4, canvas.height / 8);
+ 
+  //draw text score player 1
+  ctx.fillStyle = 'white';
+  ctx.font = '50px arial';
+  ctx.fillText(datas.score[0].toString(), canvas.width / 4 + 40, canvas.height / 4);
+  
+  //draw text player 2
+  ctx.fillStyle = 'white';
+  ctx.font = '50px arial';
+  ctx.fillText('Player 2', canvas.width / 4 * 3 - 100, canvas.height / 8);
+  
+  //draw text score player 2
+  ctx.fillStyle = 'white';
+  ctx.font = '50px arial';
+  ctx.fillText(datas.score[1].toString(), canvas.width / 4 * 3 - 70, canvas.height / 4);
+  
+  //draw net
+  for(let i = 0; i <= canvas.height; i+=15){
+    ctx.fillStyle = 'white';
+    ctx.fillRect(canvas.width / 2 - 1.5 , i, 3, 10);
+  }
+});
+var arrowUp: Boolean = false;
+var arrowDown: Boolean = false;
+var arrowLeft: Boolean = false;
+var arrowRight: Boolean = false;
+function movePlat(e: KeyboardEvent)
+{
+  if (!arrowDown)
+  {
+    if (e.key === 'ArrowDown')
     {
-      if (!arrowDown)
-      {
-        if (e.key === 'ArrowDown')
-        {
-          console.log(e.key);
-          socket.emit('ArrowDown');
-          arrowDown = true;
-        }
-      }
-      if (!arrowUp)
-      {
-        if (e.key === 'ArrowUp')
-        {
-          console.log(e.key);
-          socket.emit('ArrowUp');
-          arrowUp = true;
-        }
-      }
-      if (!arrowRight)
-      {
-        if (e.key === 'ArrowRight')
-        {
-          console.log(e.key);
-          socket.emit('ArrowRight');
-          arrowRight = true;
-        }
-      }
-      if (!arrowLeft)
-      {
-        if (e.key === 'ArrowLeft')
-        {
-          console.log(e.key);
-          socket.emit('ArrowLeft');
-          arrowLeft = true;
-        }
-      }
-    }
-
-    function stopMovePlat(e: KeyboardEvent)
-    {
-      console.log ("keyup");
       console.log(e.key);
-      if (e.key === 'ArrowDown')
-      {
-        socket.emit('ArrowDown');
-        arrowDown = false;
-      }
-      if (e.key === 'ArrowUp')
-      {
-        socket.emit('ArrowUp');
-        arrowUp = false;
-      }
-      if (e.key === 'ArrowRight')
-      {
-        socket.emit('ArrowRight');
-        arrowRight = false;
-      }
-      if (e.key === 'ArrowLeft')
-      {
-        socket.emit('ArrowLeft');
-        arrowLeft = false;
-      }
+      socket.emit('ArrowDown');
+      arrowDown = true;
     }
-
-    // function rubenpong()
-    // {
-    //   render();
-    // }
-    // const fps: number = 60;
-    // setInterval(rubenpong, 1000/fps);
-
-    document.addEventListener('keydown', movePlat);
-    document.addEventListener('keyup', stopMovePlat);
-
-
+  }
+  if (!arrowUp)
+  {
+    if (e.key === 'ArrowUp')
+    {
+      console.log(e.key);
+      socket.emit('ArrowUp');
+      arrowUp = true;
+    }
+  }
+  if (!arrowRight)
+  {
+    if (e.key === 'ArrowRight')
+    {
+      console.log(e.key);
+      socket.emit('ArrowRight');
+      arrowRight = true;
+    }
+  }
+  if (!arrowLeft)
+  {
+    if (e.key === 'ArrowLeft')
+    {
+      console.log(e.key);
+      socket.emit('ArrowLeft');
+      arrowLeft = true;
+    }
+  }
+}
+function stopMovePlat(e: KeyboardEvent)
+{
+  console.log ('keyup');
+  console.log(e.key);
+  if (e.key === 'ArrowDown')
+  {
+    socket.emit('ArrowDown');
+    arrowDown = false;
+  }
+  if (e.key === 'ArrowUp')
+  {
+    socket.emit('ArrowUp');
+    arrowUp = false;
+  }
+  if (e.key === 'ArrowRight')
+  {
+    socket.emit('ArrowRight');
+    arrowRight = false;
+  }
+  if (e.key === 'ArrowLeft')
+  {
+    socket.emit('ArrowLeft');
+    arrowLeft = false;
+  }
+}
+document.addEventListener('keydown', movePlat);
+document.addEventListener('keyup', stopMovePlat);
     //start of game define which plat - b / f?
     //update() function in backend
     //position plat, ball and player in backend?
-
     //powerups
-
   }
 };
 </script>
