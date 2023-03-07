686d222c7d687241589db2d47ec71723560cb53e
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-03-07 12:06:43 +0100

    cleaning up router a bit

--- frontend/rubenpong/src/router/index.ts
+++ frontend/rubenpong/src/router/index.ts
@@ -1,49 +1,53 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
+
+/*
+ * TODO's:
+ * 1. Add a beforeEnter hook to routes that require authentication
+ *  - import { checkAuth } from '@/auth';
+ *  - beforeEnter: checkAuth
+ * 2. Props to the ChatRoomView? <- Lindsay
+ */
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: () => import('@/views/HomeView.vue'),
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: () => import('@/views/AboutView.vue'),
+  },
+  {
+    path: '/chat',
+    name: 'chat',
+    component: () => import('@/views/ChatView.vue'),
+  },
+  {
+    path: '/game',
+    name: 'game',
+    component: () => import('@/views/GameView.vue'),
+  },
+  {
+    path: '/chatroom',
+    name: 'chatroom',
+    component: () => import('@/views/ChatRoomView.vue'),
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/LogInView.vue'),
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'not-found',
+    component: () => import('@/views/NotFoundView.vue'),
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('@/views/AboutView.vue'),
-    },
-    {
-      path: '/chat',
-      name: 'chat',
-      component: () => import('@/views/ChatView.vue'),
-    },
-    {
-      path: '/game',
-      name: 'game',
-      component: () => import('@/views/GameView.vue'),
-    },
-    {
-      path: '/chatroom',
-      name: 'chatroom',
-      component: () => import('@/views/ChatRoomView.vue'),
-      //   props: route=> ({ username: route.query.username, room: route.query.room})
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: () => import('@/views/LogInView.vue'),
-    },
-    {
-      path: '/:pathMatch(.*)*',
-      name: 'not-found',
-      component: () => import('@/views/NotFoundView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
