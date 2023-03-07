ed2b7ffd1fd72b3b7fc9bcad7f0fcc197423eb6b
Ruben van Meerten <ruben.vanmeerten@hotmail.nl>
2023-03-07 17:49:24 +0100

    Adds a proper MatchmakingService class

--- backend/volume/src/game/matchmaking.service.ts
+++ backend/volume/src/game/matchmaking.service.ts
@@ -1,42 +1,63 @@
 import { GameService } from './game.service';
 import { GameMode } from './game.service';
 
-class QueuedUser {
-  constructor (mode: GameMode, userId: number) {
-    this.mode = mode;
-    this.userId = userId;
-  }
-  mode: GameMode;
-  userId: number;
-}
-
 export class MatchmakingService {
-  playerQueue: QueuedUser[];
+  constructor (gameService: GameService) {
+    this.gameService = gameService;
+  }
+
+  gameService: GameService;
+  playerQueueNormal: number[] = [];
+  playerQueueFreeMove: number[] = [];
+  playerQueuePowerUp: number[] = [];
+  playerQueueFiesta: number[] = [];
 
   addPlayerToQueue(mode: GameMode, userId: number) {
-    const newplayer = new QueuedUser(mode, userId);
-
-    this.playerQueue.push(newplayer);
+    if (mode === GameMode.NORMAL)
+      this.playerQueueNormal.push(userId);
+    else if (mode === GameMode.FREEMOVE)
+      this.playerQueueFreeMove.push(userId);
+    else if (mode === GameMode.POWERUP)
+      this.playerQueuePowerUp.push(userId);
+    else if (mode === GameMode.FIESTA)
+      this.playerQueueFiesta.push(userId);
   }
 
   removePlayerFromQueue(userId: number) {
-    for (let index = 0; index < this.playerQueue.length; index++) {
-      const player = this.playerQueue[index];
-      
-      if (player.userId === userId) {
-        this.playerQueue.splice(index, 1);
-        return;
-      }
-    }
+    if (this.checkAndRemoveFromArray(this.playerQueueNormal, userId))
+      return;
+    if (this.checkAndRemoveFromArray(this.playerQueueFreeMove, userId))
+      return;
+    if (this.checkAndRemoveFromArray(this.playerQueuePowerUp, userId))
+      return;
+    if (this.checkAndRemoveFromArray(this.playerQueueFiesta, userId))
+      return;
   }
 
-  checkForMatch() {
-    if (this.playerQueue.length < 2)
+  private checkAndRemoveFromArray(arr: number[], userId: number) {
+    const index = arr.indexOf(userId);
+
+    if (index === -1)
+      return false;
+    arr.splice(index, 1);
+    return true;
+  }
+
+  private checkAndMatchPlayers(arr: number[], mode: GameMode) {
+    if (arr.length < 2)
       return;
 
-    for (let index = 0; index < this.playerQueue.length; index++) {
-      const player = this.playerQueue[index];
-      
-    }
+    const player1 = arr.pop();
+    const player2 = arr.pop();
+    this.gameService.createGame(player1, player2, mode);
+
+    // send info to players that game is created
+  }
+
+  checkForMatches() {
+    this.checkAndMatchPlayers(this.playerQueueNormal, GameMode.NORMAL);
+    this.checkAndMatchPlayers(this.playerQueueFreeMove, GameMode.FREEMOVE);
+    this.checkAndMatchPlayers(this.playerQueuePowerUp, GameMode.POWERUP);
+    this.checkAndMatchPlayers(this.playerQueueFiesta, GameMode.FIESTA);
   }
 }
