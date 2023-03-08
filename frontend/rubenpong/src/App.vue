0659d590ae92cd07219256fa685805416e9aae5d
Lindsay <55634165+LJmartin94@users.noreply.github.com>
2023-03-08 16:49:27 +0100

    Update App.vue
    Make Lindsay (picky one) happy

--- frontend/rubenpong/src/App.vue
+++ frontend/rubenpong/src/App.vue
@@ -59,7 +59,6 @@ import HelloWorld from './components/HelloWorld.vue';
         <RouterLink to="/login">
           Log in
         </RouterLink>
-        <!-- <RouterLink to="/canvas">Canvas</RouterLink> -->
       </nav>
     </div>
   </header>
