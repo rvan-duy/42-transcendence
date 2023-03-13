92e6467b0afce1bce0e3ee6094646a30ad5f5aa8
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-03-13 16:38:16 +0100

    Enable a hacky multiplayer mode

--- backend/volume/src/game/game.gateway.ts
+++ backend/volume/src/game/game.gateway.ts
@@ -27,22 +27,31 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   private server: Server;
   private gameService: GameService;
   private currGameState: CurrentGameState;
+  private players: number = 0;
+  private player_clients: Array<string> = [];
+  private id: number;
 
   afterInit(server: Server) {
     this.server = server;
     this.gameService = new GameService(this.server);
-    this.gameService.createGame(1, 2, GameMode.FREEMOVE);
     const fps: number = 60;
     setInterval(function() {this.gameService.updateGames();}.bind(this), 1000/fps);
-  }
+}
 
-  handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+handleConnection(client: Socket) {
+	console.log(`Client connected: ${client.id}`);
     client.emit('init'); // new connection
+	this.players++;
+	console.log(`Client connected: ${client}`);
+	this.player_clients.push(client.id);
+	if(this.players % 2 == 0)
+		this.gameService.createGame(1, 2, GameMode.FREEMOVE);
   }
 
   handleDisconnect(client: Socket) {
+	// TODO: Set game finished bool to true, make other player win.
     console.log(`Client disconnected: ${client.id}`);
+	this.players--;
   }
 
   @SubscribeMessage('pos')
@@ -53,24 +62,29 @@ export class GameGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
 
   @SubscribeMessage('ArrowDown')
   handleKeyDown(client: any, payload: any) {
-    this.gameService.UpdatePlayerInput(1, PaddleInput.DOWN); // magic
+	this.id = this.player_clients.indexOf(client.id) % 2 + 1;
+	console.log(this.id);
+	this.gameService.UpdatePlayerInput(this.id, PaddleInput.DOWN); // magic
     console.warn(`client ${client} and payload ${payload} unused`);
   }
 
   @SubscribeMessage('ArrowUp')
   handleKeyUp(client: any, payload: any) {
-    this.gameService.UpdatePlayerInput(1, PaddleInput.UP); // magic
+	this.id = this.player_clients.indexOf(client.id) % 2 + 1;
+    this.gameService.UpdatePlayerInput(this.id, PaddleInput.UP); // magic
     console.warn(`client ${client} and payload ${payload} unused`);
   }
 
   @SubscribeMessage('ArrowLeft')
   handleKeyLeft(client: any, payload: any) {
-    this.gameService.UpdatePlayerInput(1, PaddleInput.LEFT); // magic
+	this.id = this.player_clients.indexOf(client.id) % 2 + 1;
+    this.gameService.UpdatePlayerInput(this.id, PaddleInput.LEFT); // magic
     console.warn(`client ${client} and payload ${payload} unused`);
   }
   @SubscribeMessage('ArrowRight')
   handleKeyRight(client: any, payload: any) {
-    this.gameService.UpdatePlayerInput(1, PaddleInput.RIGHT); // magic
+	this.id = this.player_clients.indexOf(client.id) % 2 + 1;
+    this.gameService.UpdatePlayerInput(this.id, PaddleInput.RIGHT); // magic
     console.warn(`client ${client} and payload ${payload} unused`);
   }
 
