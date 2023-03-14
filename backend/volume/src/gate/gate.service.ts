506f8846bed8ffb1654c520a28185a6bef1a0376
Oswin Bult <obult@student.codam.nl>
2023-03-14 19:02:15 +0100

    change chat sockets into an array and add single socket for game
    game sockets might only need one (unsure bc playing the game) while chatsockets could be open on diferent tabs with different sockets

--- backend/volume/src/gate/gate.service.ts
+++ backend/volume/src/gate/gate.service.ts
@@ -1,21 +1,52 @@
 import { Injectable } from "@nestjs/common";
 import { Socket } from "dgram";
 
+interface socketContainer {
+  gameSocket: Socket,
+  chatSockets: Socket[],
+}
 
 @Injectable()
 export class GateService {
   constructor(){}
-  private usersOnline: Map<number, Socket>;
+  private usersOnline: Map<number, socketContainer>;
 
-  userComesOnline(userId: number, sock: Socket) {
-    this.usersOnline.set(userId, sock);
+  addChatSocket(userId: number, sock: Socket) {
+    let tmpSock: socketContainer;
+    if (this.usersOnline.has(userId)) {
+      tmpSock = this.usersOnline[userId]
+      tmpSock.chatSockets.push(sock);
+      this.usersOnline.set
+    } else {
+      tmpSock = {
+        gameSocket: undefined,
+        chatSockets: [sock],
+      }
+    }
+    this.usersOnline.set(userId, tmpSock);
   }
 
-  userGoesOffline(userId: number) {
-    this.usersOnline.delete(userId);
+  setGameSocket(userId: number, sock: Socket) {
+    let tmpSock: socketContainer;
+    tmpSock = this.usersOnline.get(userId);
+    tmpSock.gameSocket = sock;
+    this.usersOnline.set(userId, tmpSock);
   }
 
-  async getUserSocket(userId: number): Promise<Socket>  {
-    return this.usersOnline[userId];
+  deleteChatSocket(userId: number, sock: Socket) {
+    let container = this.usersOnline.get(userId);
+    const index = container.chatSockets.indexOf(sock);
+    container.chatSockets.splice(index, 1);
+    this.usersOnline.set(userId, container);
+  }
+
+  async getUserGameSocket(userId: number): Promise<Socket>  {
+    const container = this.usersOnline.get(userId);
+    return container.gameSocket;
+  }
+
+  async getUserChatSockets(userId: number): Promise<Socket[]> {
+    const container = this.usersOnline.get(userId);
+    return container.chatSockets;
   }
 }
