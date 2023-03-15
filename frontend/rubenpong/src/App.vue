0222579919771b54b3ed5d9f0f295d909568b9a5
Dagmar Kramer <dkramer@f0r6s2.codam.nl>
2023-03-15 11:27:47 +0100

    hardcoded user icon added

--- frontend/rubenpong/src/App.vue
+++ frontend/rubenpong/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
@@ -6,7 +7,7 @@
     <div class="flex items-center flex-shrink-0 text-white mr-6">
       <font-awesome-icon icon="table-tennis-paddle-ball" />    <span class="p-2 font-semibold text-xl tracking-tight">RubenPong</span>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="w-1/2 block flex-grow lg:flex lg:items-center lg:w-auto">
       <div class="text-sm lg:flex-grow">
         <RouterLink
           class="text-blue-100 p-2 text-lg hover:text-white"
@@ -32,6 +33,11 @@
         >
           Log Out
         </RouterLink>
+        
+      </div>
+      <div class="columns-1">
+      <img src="./assets/dagmar.jpeg" width="50" height="50" class="pr-2" style="border-radius: 50%; float: right;"/>
+      <div><p class="text-white text-xs">dkramer</p></div>
       </div>
     </div>
   </nav>
@@ -41,7 +47,29 @@
 </template>
 <script lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
+export default {
 
+  data()
+  {
+    return {
+      selectGameMode: false,
+      matched: false,
+      gameMode : '',
+      name: '',
+      
+    };
+  },
+  mounted() {
+  fetch('http://localhost:3000/user/me')
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      this.name = data.name;
+      console.log(data);
+    });
+  }
+}
 </script>
 
 <style src="./assets/main.css"></style>
