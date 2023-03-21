29d300a914e6c4a4e4544f83f0dc5e5d5b72151c
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-03-21 16:00:20 +0100

    new way of reading .env

--- backend/volume/src/auth/forty-two.strategy.ts
+++ backend/volume/src/auth/forty-two.strategy.ts
@@ -9,7 +9,7 @@ export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
     super({
       clientID: process.env.APPLICATION_ID,
       clientSecret: process.env.APPLICATION_SECRET,
-      callbackURL: process.env.REDIRECT_URI,
+      callbackURL: `http://${process.env.CODAM_PC}/auth/callback`,
       profileFields: {
         'id': function (obj: any) { return Number(obj.id); },
         'username': 'login',
@@ -19,8 +19,6 @@ export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
 
   async validate(accessToken: string, refreshToken: string, profile: any, cb: Function) {
     console.log('validating user', profile.username);
-    console.log('accessToken', accessToken);
-    console.log('refreshToken', refreshToken);
     const user = await this.authService.validateUser(profile.id, profile.username);
     if (!user) {
       throw new UnauthorizedException();
