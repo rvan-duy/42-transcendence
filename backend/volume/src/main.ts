c2000375aa5382a18ebe61915ba6cdcd8adc0a5b
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-03-23 18:01:09 +0100

    The Networking branch continues to haunt this project.

--- backend/volume/src/main.ts
+++ backend/volume/src/main.ts
@@ -14,8 +14,20 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, { cors: true });
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
+  const allowed_origins: string[] = ['*', 'https://api.intra.42.fr', 'http://localhost:8000', 'http://127.0.0.1:8000'];
+  for (let f=0; f<2; f++)
+  {
+    for(let r=1; r<7; r++)
+    {
+      for(let s=1; s<30; s++)
+      {
+        const dns_for_imac = 'http://f'+f+'r'+r+'s'+s+'.codam.nl:8000';
+        allowed_origins.push(dns_for_imac);
+      }
+    }
+  }
   app.enableCors( {
-    origin: ['*', 'http://f0r5s15.codam.nl:8000', 'http://f0r5s15.codam.nl:3000', 'https://api.intra.42.fr', 'http://localhost:8000'],
+    origin: allowed_origins,
     methods: ['GET', 'POST', 'PUT'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
