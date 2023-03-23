617930db89e9225b6e2ffba4585b1288e99f2e94
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-03-23 18:01:34 +0100

    Removed comment with previous implementation.

--- frontend/rubenpong/src/views/ChatRoomView.vue
+++ frontend/rubenpong/src/views/ChatRoomView.vue
@@ -18,23 +18,13 @@ async function getUserInfo(){
   }
   else{
     console.log('Fetching user info');
-	await getBackend('user/me')
+    await getBackend('user/me')
       .then((response => response.json()))
       .then((data) => {
-		username = data.name;
+        username = data.name;
         id = data.id;
         intraId = data.intraId;
       }).catch(error => console.log('Failed to fetch user : ' + error.message));
-
-    // await fetch('http://localhost:3000/user/me', {mode: 'cors'})
-    //   .then(async function(res)
-    //   {
-    //     var data = await res.json();
-    //     username = data.name;
-    //     id = data.id;
-    //     intraId = data.intraId;
-    //   })
-    //   .catch(error => console.log('Failed to fetch user : ' + error.message));
   }
   //   console.log(`Username returned by getUsername: ${username}`);
   return {username, id, intraId};
