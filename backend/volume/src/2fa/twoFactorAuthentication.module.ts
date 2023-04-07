c6b3d9a80cc9f8277279c0ba8720e7b18008d634
Ruben van Duyneveldt <rvanduyneveldt@gmail.com>
2023-04-07 16:09:58 +0200

    added missing import

--- backend/volume/src/2fa/twoFactorAuthentication.module.ts
+++ backend/volume/src/2fa/twoFactorAuthentication.module.ts
@@ -4,10 +4,15 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 import { PrismaUserService } from 'src/user/prisma/prismaUser.service';
 import { TwoFactorAuthenticationController } from './twoFactorAuthentication.controller';
 import { AuthService } from 'src/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     PrismaModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
   ],
   controllers: [
     TwoFactorAuthenticationController,
