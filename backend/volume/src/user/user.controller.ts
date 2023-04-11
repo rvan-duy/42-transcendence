e670cc50a0faceb306741889653455c8c523419e
Oswin Bult <obult@student.codam.nl>
2023-04-11 20:56:55 +0200

    handled base pfb setting with post requests

--- backend/volume/src/user/user.controller.ts
+++ backend/volume/src/user/user.controller.ts
@@ -93,33 +93,116 @@ export class UserController {
 
     res.status(HttpStatus.OK).send(users);
   }
-
+  
+  // is this user blocked by other user?
+    // do not do anything maybe return a negative response
+  // did the other user already send a friend request?
+    // add as friends on both sides
+  // set pending on this side
   @UseGuards(JwtAuthGuard)
   @Post('befriend')
   async handleFriendRequest(@Request() req: any, @Query('id') userId: number) {
-    // is this user blocked by other user?
-      // do not do anything maybe return a negative response
-    // did the other user already send a friend request?
-      // add as friends on both sides
-    // set pending on this side
+    const myId = req.user.id;
+    const meAsUser = await this.userService.user({id: myId});
+    const otherAsUser = await this.userService.user({id: userId});
+    if (otherAsUser === undefined)
+      return ;
+    if (otherAsUser.blocked.includes(myId))
+      return ; // they blocked you do not try and freind!!
+    if (meAsUser.blocked.includes(userId))
+      return ; // you blocked them do not be silly!
+    if (meAsUser.pending.includes(userId))
+    {
+      meAsUser.friends.push(userId);
+      meAsUser.pending.splice(meAsUser.pending.indexOf(userId), 1); // removes the pending request
+      otherAsUser.friends.push(myId);
+      this.userService.updateUser({
+      where: {
+        id: myId,
+        },
+      data: {
+        friends: meAsUser.friends,
+        }
+      })
+      return ; // well done you are now friends
+    }
+    otherAsUser.pending.push(myId);
+    this.userService.updateUser({
+      where: {
+        id: otherAsUser.id,
+      },
+      data: {
+        pending: otherAsUser.pending,
+      }
+    })
+    return ; // wait till they accept your request (spannend!)
   }
 
+  // not needed
+  // unfriends removes each other from friends onboth sides
   @UseGuards(JwtAuthGuard)
   @Post('unfriend')
   async handleUnfriend(@Request() req: any, @Query('id') userId: number) {
-    // remove as friends from both sides
+    const myId = req.user.id;
+    const meAsUser = await this.userService.user({id: myId});
+    const otherAsUser = await this.userService.user({id: userId});
+    if (meAsUser.friends.includes(userId) === false)
+      return ; // you are not friends!
+    meAsUser.friends.splice(meAsUser.friends.indexOf(userId), 1); // removes the friend :(
+    otherAsUser.friends.splice(otherAsUser.friends.indexOf(myId), 1); // removes the friend :(
+    this.userService.updateUser({
+      where: {
+        id: myId,
+      },
+      data: {
+        friends: meAsUser.friends,
+      }
+    });
+    this.userService.updateUser({
+      where: {
+        id: userId,
+      },
+      data: {
+        friends: otherAsUser.friends,
+      }
+    })
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('block')
   async handleBlock(@Request() req: any, @Query('id') userId: number) {
+    const myId = req.user.id;
     // add to block on this side
+    const meAsUser = await this.userService.user(myId);
+    meAsUser.blocked.push(userId);
+    this.userService.updateUser({
+      where: {
+        id: myId,
+      },
+      data: {
+        blocked: meAsUser.blocked,
+      },
+    })
     // unfriend in case they where friends
+    this.handleUnfriend(req, userId);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('unblock')
   async handleUnBlock(@Request() req: any, @Query('id') userId: number) {
+    const myId = req.user.id;
+    const meAsUser = await this.userService.user(myId);
+    if (meAsUser.blocked.includes(userId) === false)
+      return ; // you did not blcok them
     // unblock on this users side
+    meAsUser.blocked.splice(meAsUser.blocked.indexOf(userId), 1);
+    this.userService.updateUser({
+      where: {
+        id: myId,
+      },
+      data: {
+        blocked: meAsUser.blocked,
+      },
+    })
   }
 }
