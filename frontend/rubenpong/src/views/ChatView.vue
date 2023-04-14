4b7c0ff5c55e43716554f5e6197bf1d1d8c1f294
LJmartin94 <lindsayjamesmartin94@gmail.com>
2023-04-15 00:04:10 +0200

    Make the chat forms be forms again, not divs.
    Prevented the submit behaviour from refreshing the page without changing 'form' to 'div'. Now as an added bonus, if the user doesn't fill in a required field the button will prompt them to fill in the missing field instead of doing nothing. This is part of default form submission behaviour.

--- frontend/rubenpong/src/views/ChatView.vue
+++ frontend/rubenpong/src/views/ChatView.vue
@@ -10,48 +10,58 @@
           <h1><i class="fas fa-smile" /> RubenSpeak</h1>
         </header>
         <main class="join-main">
-
-          <div v-if="chatCreate === false">
-            <div class="form-control">
-              <label for="room">Room</label>
-              <select
-                id="room"
-                v-model="selectedChat"
-                class="text-black"
-                name="room"
-                style="border-radius: 20px"
-              >
-                <option
-                  v-for="chat in chats"
-                  :key="chat.id"
-                  :value="chat"
+          <form @submit.prevent>
+            <div v-if="chatCreate === false">
+              <div class="form-control">
+                <label for="room">Room</label>
+                <select
+                  id="room"
+                  v-model="selectedChat"
+                  class="text-black"
+                  name="room"
+                  style="border-radius: 20px"
+                  required
                 >
-                  {{ chat.name }}
-                </option>
-              </select>
+                  <option
+                    v-for="chat in chats"
+                    :key="chat.id"
+                    :value="chat"
+                  >
+                    {{ chat.name }}
+                  </option>
+                </select>
+              </div>
+              <div v-if="selectedChat?.access === 'PROTECTED'">
+                <label for="name">Password</label>
+                <span class="text-black pr-4"><input
+                  id="username"
+                  v-model="enteredPW"
+                  VALYE
+                  type="text"
+                  name="username"
+                  placeholder="Enter password..."
+                  required
+                  style="border-radius: 20px; width:300px; font-size: 12px; height: 35px;"
+                > </span>
+              </div>
+              <!-- We need a backend check to see if password is valid, but I implemented a frontend check to make user check whether they typed a password if one is required -->
+              <button
+                v-if="selectedChat == NULL || (selectedChat?.access === 'PROTECTED' && enteredPW == '' )"
+                type="submit"
+                class="btn bg-blue-100"
+              >
+                Join Chat
+              </button>
+              <button
+                v-else
+                type="submit"
+                class="btn bg-blue-100"
+                @click="goTo('chatroom/' + selectedChat?.name + '?id=' + selectedChat?.id)"
+              >
+                Join Chat
+              </button>
             </div>
-            <div v-if="selectedChat?.access === 'PROTECTED'">
-              <label for="name">Password</label>
-              <span class="text-black pr-4"><input
-                id="username"
-                v-model="enteredPW"
-                VALYE
-                type="text"
-                name="username"
-                placeholder="Enter password..."
-                required
-                style="border-radius: 20px; width:300px; font-size: 12px; height: 35px;"
-              > </span>
-            </div>
-            <!-- We need a backend check to see if password is valid, but I implemented a frontend check to make user check whether they typed a password if one is required -->
-            <button
-              :disabled="selectedChat?.name === '' || (selectedChat?.access === 'PROTECTED' && enteredPW == '' )"
-              class="btn bg-blue-100"
-              @click="goTo('chatroom/' + selectedChat?.name + '?id=' + selectedChat?.id)"
-            >
-              Join Chat
-            </button>
-          </div>
+          </form>
           <button
             v-if="chatCreate === false"
             class="btn bg-blue-500 text-white"
@@ -59,71 +69,81 @@
           >
             Create Chat
           </button>
-          <div
-            v-if="chatCreate === true"
-            class="form-control"
-          >
-            <label for="room">Select a Chat Type</label>
-            <!-- this needs to change -->
-            <select
-              id="chat_access"
-              v-model="newChat.access"
-              class="text-black"
-              name="room"
-              style="border-radius: 20px"
-              required
+          <form @submit.prevent>
+            <div
+              v-if="chatCreate === true"
+              class="form-control"
             >
-              <option value="PRIVATE">
-                Private
-              </option>
-              <option value="PUBLIC">
-                Public
-              </option>
-              <option value="PROTECTED">
-                With Password
-              </option>
-            </select>
+              <label for="room">Select a Chat Type</label>
+              <!-- this needs to change -->
+              <select
+                id="chat_access"
+                v-model="newChat.access"
+                class="text-black"
+                name="room"
+                style="border-radius: 20px"
+                required
+              >
+                <option value="PRIVATE">
+                  Private
+                </option>
+                <option value="PUBLIC">
+                  Public
+                </option>
+                <option value="PROTECTED">
+                  With Password
+                </option>
+              </select>
            
-            <label
-              for="name"
-              class="pt-2"
-            >Chat name</label>
-            <span class="text-black pr-4"><input
-              id="chatName"
-              v-model="newChat.name"
-              VALYE
-              type="text"
-              name="username"
-              placeholder="Enter a name for the chat"
-              required
-              style="border-radius: 20px; width:300px; font-size: 12px; height: 35px;"
-            > </span>
-            <div v-if="newChat.access === 'PROTECTED'">
               <label
                 for="name"
                 class="pt-2"
-              >Password</label>
+              >Chat name</label>
               <span class="text-black pr-4"><input
-                id="password"
-                v-model="newChat.password"
+                id="chatName"
+                v-model="newChat.name"
                 VALYE
                 type="text"
                 name="username"
-                placeholder="Enter a password for the chat"
+                placeholder="Enter a name for the chat"
                 required
                 style="border-radius: 20px; width:300px; font-size: 12px; height: 35px;"
               > </span>
-            </div>
-            {{ newChat }}
-            <button
-              :disabled="newChat.name == NULL || newChat.access === undefined || ( newChat.access === 'PROTECTED'&& newChat.password == NULL)"
-              class="btn bg-blue-100"
-              @click="createChat(newChat)"
-            >
-              Create Chat
-            </button>
-          </div>
+              <div v-if="newChat.access === 'PROTECTED'">
+                <label
+                  for="name"
+                  class="pt-2"
+                >Password</label>
+                <span class="text-black pr-4"><input
+                  id="password"
+                  v-model="newChat.password"
+                  VALYE
+                  type="text"
+                  name="username"
+                  placeholder="Enter a password for the chat"
+                  required
+                  style="border-radius: 20px; width:300px; font-size: 12px; height: 35px;"
+                > </span>
+              </div>
+              {{ newChat }}
 
+              <button
+                v-if="newChat.name == NULL || newChat.access === undefined || ( newChat.access === 'PROTECTED'&& newChat.password == NULL)"
+                type="submit"
+                class="btn bg-blue-100"
+              >
+                Create Chat
+              </button>
+              <button
+                v-else
+                type="submit"
+                class="btn bg-blue-100"
+                @click="createChat(newChat)"
+              >
+                Create Chat
+              </button>
+            </div>
+          </form>
         </main>
       </div>
     </body>
